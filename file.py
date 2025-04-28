f01666737298d330cba8c02cd90aa51f30836458
unknown <xnovak11@ad.fi.muni.cz>
2025-04-28 17:01:00 +0200

    fix 1

--- file.py
+++ file.py
@@ -1,7 +1,11 @@
 import os
 
+"""
+Function for running user code
+"""
 def run_code(user_code):
-    eval(user_code)  # Codacy will flag this as dangerous
+    eval(user_code)
+
 
 user_input = input("Enter a filename: ")
 os.system("cat " + user_input)
